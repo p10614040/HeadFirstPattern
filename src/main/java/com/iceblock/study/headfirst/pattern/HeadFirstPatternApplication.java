c1abf5aba75a7453fb7cb3e9b22c258a6e45df83
严亮 <695795023@qq.com>
2017-08-21 20:57:35 +0800

    初始工程提交

---
+++ src/main/java/com/iceblock/study/headfirst/pattern/HeadFirstPatternApplication.java
@@ -0,0 +1,12 @@
+package com.iceblock.study.headfirst.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadFirstPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadFirstPatternApplication.class, args);
+	}
+}
